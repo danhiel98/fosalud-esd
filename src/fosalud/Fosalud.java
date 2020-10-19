a271791183abb676aad55c0c543a8a91d7f17023
Daniel García <danhiel.garcia98@gmail.com>
2020-10-18 23:12:18 -0600

    Commit inicial

---
+++ src/fosalud/Fosalud.java
@@ -0,0 +1,12 @@
+package fosalud;
+
+public class Fosalud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
