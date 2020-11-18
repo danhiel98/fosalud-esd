85c5c5bdd50c07b35aefd56612dcc5d865b7aba1
NeoPunk <joaquinjuarez17@hotmail.com>
2020-11-17 22:05:58 -0600

    Admin class created

---
+++ src/fosalud/Administrar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fosalud;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author NeoPunk
+ */
+public class Administrar {
+    
+    LinkedList<Unidad> unidad= new LinkedList<>();
+    
+    public void AgregarUnuidad(){
+        
+    }
+    
+}
