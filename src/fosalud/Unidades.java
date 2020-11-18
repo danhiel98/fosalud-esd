640053d490f5eb2b00de2cc51e9d262fea0a14a6
NeoPunk <joaquinjuarez17@hotmail.com>
2020-11-17 22:47:52 -0600

    Fixed Version

---
+++ src/fosalud/Unidades.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fosalud;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class Unidades {
+    
+    String nombre, departamento,municipio, telefono;
+
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+}
